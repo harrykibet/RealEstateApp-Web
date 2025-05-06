914509f168c0968242512c84bdbf237215f71d2d
harrykibet <trmnjames@gmail.com>
2025-05-06 20:40:05 +0300

    update node package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
+  test: {
+    include: ['src/tests/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    environment: 'jsdom', // optional, for React DOM,
+     passWithNoTests: true,
   }
 })
+
